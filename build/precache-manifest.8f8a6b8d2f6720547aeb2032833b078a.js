ed5c9bb5cc24357155c81f66b21869f871a57d04
Vitor Freitas <vitorldfreitas@gmail.com>
2019-03-26 22:38:14 -0300

    production build

---
+++ build/precache-manifest.8f8a6b8d2f6720547aeb2032833b078a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a4a62bf7b7aacec4c137",
+    "url": "/static/js/main.a4a62bf7.chunk.js"
+  },
+  {
+    "revision": "0ce7444450e66d4d63d3",
+    "url": "/static/js/2.0ce74444.chunk.js"
+  },
+  {
+    "revision": "f3210a75aadfb0e0a4887bbe8aba83aa",
+    "url": "/index.html"
+  }
+];
